70fd9abd402016b6e00b1cc3d31101fc71ce5fda
kamilbrzakala <bzykson@gmail.com>
2020-09-08 22:12:16 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+//Write a program that reads an array of lowercase strings
+// and checks whether the array is in alphabetical order or not.
+
+
+
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+
+        final Scanner scanner = new Scanner(System.in);
+        String[] array = scanner.nextLine().split(" ");
+        boolean isTrue = true;
+
+        for (int i = 0; i < array.length; i++) {
+            String first = array[i];
+            String second = "";
+            try {
+                second = array[i + 1];
+            } catch (Exception ex) {
+                second = array[i];
+                break;
+            }
+
+//            System.out.println("first: " + first);
+//            System.out.println("second: " + second);
+//            System.out.println("first to second: " + first + " second: "+ second + " result: " + first.compareTo(second));
+
+            if (first.compareTo(second) <= 0) {
+                isTrue = true;
+
+            } else {
+
+                isTrue = false;
+                break;
+
+            }
+        }
+        System.out.println(isTrue);
+    }
+}
