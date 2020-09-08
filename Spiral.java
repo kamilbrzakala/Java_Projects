9eca1ddc24fc6d3fd010206726e5741bb0a26088
kamilbrzakala <bzykson@gmail.com>
2020-09-08 22:04:14 +0200

    Update and rename Main.java to Spiral.java

--- Spiral.java
+++ Spiral.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 //        13 12 11 10 9
 
 
-class Main {
+class Spiral {
     public static void main(String[] args) {
         // put your code here
         Scanner scanner = new Scanner(System.in);
